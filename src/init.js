5dc7da71279857bb671f24701b63ec0ff8b5b367
rhinodavid <dawalsh@gmail.com>
2016-09-19 14:51:50 -0700

    Refactor to use ES6 classes

--- src/init.js
+++ src/init.js
@@ -15,18 +15,19 @@ $(document).ready(function() {
      * A new object of the given type will be created and added
      * to the stage.
      */
-    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+    var dancerName = $(this).data('dancer-name');
 
     // get the maker function for the kind of dancer we're supposed to make
-    var dancerMakerFunction = window[dancerMakerFunctionName];
+    var dancerMakerFunction = window[dancerName];
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+
     $('body').append(dancer.$node);
   });
 });

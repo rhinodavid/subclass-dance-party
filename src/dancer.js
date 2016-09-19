e52a70c03a1bda63da7e00e8a0b7915c52e45958
rhinodavid <dawalsh@gmail.com>
2016-09-19 16:11:27 -0700

    Add defaultHead to Dancer

--- src/dancer.js
+++ src/dancer.js
@@ -1,9 +1,10 @@
 class Dancer {
-  constructor (top, left, timeBetweenSteps) {
+  constructor (top, left, timeBetweenSteps, defaultHead) {
     this.$node = $('<span class="dancer"></span>');
     this.setPosition(top, left);
     this.timeBetweenSteps = timeBetweenSteps;
     this.step();
+    this.defaultHead = defaultHead;
   }
   step () {
     setTimeout(this.step.bind(this), this.timeBetweenSteps);

f233c171994f3e7e5b7a89280277679405149981
rhinodavid <dawalsh@gmail.com>
2016-09-20 12:14:12 -0700

    Fix double quotes

--- src/init.js
+++ src/init.js
@@ -23,8 +23,8 @@ $(document).ready(function() {
     // make a dancer with a random position
 
     var dancer = new dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       435 * 2 // 140 bpm
     );
 

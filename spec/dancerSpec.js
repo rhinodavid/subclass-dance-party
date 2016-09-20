6bb7fb385b7cc1c077affe8af268535aee25f750
rhinodavid <dawalsh@gmail.com>
2016-09-19 19:52:08 -0700

    Add naeDancer and whipDancer tests

--- spec/dancerSpec.js
+++ spec/dancerSpec.js
@@ -1,22 +1,34 @@
+describe('Dancer', function() {
 
-describe('Dancer', function () {
-  var dancer, clock;
+  var whipDancer, clock;
   var timeBetweenSteps = 100;
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    dancer = new Dancer (10, 20, timeBetweenSteps, '&#x1f608');
+    whipDancer = new WhipDancer(10, 20, timeBetweenSteps);
   });
 
-  it('should have a default head', function () {
-    expect(dancer.defaultHead).to.equal('&#x1f608');
+  it('should have a jQuery $node object', function() {
+    expect(whipDancer.$node).to.be.an.instanceof(jQuery);
   });
 
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(whipDancer, 'step');
+      expect(whipDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(whipDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(whipDancer.step.callCount).to.be.equal(2);
+    });
+  });
 });
 
 describe('whipDancer', function() {
-  var whipDancer;
-  var clock;
+  var whipDancer, clock;
   var timeBetweenSteps = 100;
 
   beforeEach(function() {
@@ -25,6 +37,37 @@ describe('whipDancer', function() {
   });
   it('should have a jQuery $node object', function() {
     expect(whipDancer.$node).to.be.an.instanceof(jQuery);
+  });
+  it('should have a default head', function () {
+    expect(whipDancer.head).to.equal('&#x1f600');
+  });
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(whipDancer, 'step');
+      expect(whipDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(whipDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(whipDancer.step.callCount).to.be.equal(2);
+    });
+  });
+
+describe('naeDancer', function() {
+  var naeDancer, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    whipDancer = new WhipDancer(10, 20, timeBetweenSteps);
+  });
+  it('should have a jQuery $node object', function() {
+    expect(whipDancer.$node).to.be.an.instanceof(jQuery);
+  });
+  it('should have a default head', function () {
+    expect(whipDancer.head).to.equal('&#x1f600');
   });
   describe('dance', function() {
     it('should call step at least once per second', function() {

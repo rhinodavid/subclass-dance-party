ab720b0ba2c7d134cf5926b2a3b2ad237768a994
rhinodavid <dawalsh@gmail.com>
2016-09-20 12:14:36 -0700

    Add methods to make dancer move

--- src/dancer.js
+++ src/dancer.js
@@ -22,6 +22,8 @@ class Dancer {
       top: top,
       left: left
     };
+    this._x = left;
+    this._y = top;
     this.$node.css(styleSettings);
   }
   setHead(head) {
@@ -30,5 +32,39 @@ class Dancer {
   }
   setPose(html) {
     this.$body.html(html);
+  }
+  moveTo(x, y, time = 5000) {
+    this._targetX = x;
+    this._targetY = y;
+    var distanceX = this._targetX - this._x;
+    var distanceY = this._targetY - this._y;
+    this._frameRate = 33; //33 = 30fps
+    var frames = time / this._frameRate;
+    var frameX = distanceX / frames; 
+    var frameY = distanceY / frames;
+    this._move(frameX, frameY);
+  }
+
+  _move(frameX, frameY) {
+    var distanceX = this._targetX - this._x;
+    var distanceY = this._targetY - this._y;
+    var newX, newY;
+    if (Math.abs(distanceX) < Math.abs(frameX)) {
+      newX = this._targetX;
+    }
+    if (Math.abs(distanceY) < Math.abs(frameY)) {
+      newY = this._targetY;
+    }
+    if (newX === this._targetX && newY === this._targetY) {
+      // the dancer has made it to the new spot
+      this.setPosition(newX, newY);
+    } else {
+      // this dancer has not made it
+      newX = this._x + frameX;
+      newY = this._y + frameY;
+      this.setPosition(newY, newX);
+      setTimeout(this._move.bind(this, frameX, frameY), this._frameRate);
+    }
+
   }
 }
